ee18e0cd2732a5bf6a2e16fa759a535a7e76f607
Misbah128 <109318564+Misbah128@users.noreply.github.com>
2022-08-14 14:47:38 -0500

    Add files via upload

---
+++ Lab 5 P2.py
@@ -0,0 +1,10 @@
+#Syed Hussain
+#08/05/2022
+#Writing a loop that prints each of the numbers on a new line.
+Number = [12, 10, 32, 3, 66, 17, 42, 99]
+for i in Number:
+    print(i)
+#Writing a loop that prints each number and its square on a new line.
+for i in Number:
+    print("The square of",i, "is", i**2)
+    
